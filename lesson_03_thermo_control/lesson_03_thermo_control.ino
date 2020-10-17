fd1ad0089df51563dc30fcc45d6a3d0a1beb07e8
Alexey <zhuravel.lesha2005@gmail.com>
2020-10-17 09:50:50 +0300

    Thermo sensor control is added

---
+++ lesson_03_thermo_control/lesson_03_thermo_control.ino
@@ -0,0 +1,29 @@
+#define THERMO_PIN A4
+#define U_SOURCE 5
+#define R     100000
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+int adc_signal = 0;
+float adc_to_volt = 0.0;
+float tr = 0.0
+
+void loop() {
+  adc_signal = analogRead(THERMO_PIN);
+
+  adc_to_volt = U_SOURCE / 1023 * adc_signal;
+
+  tr = R * (1023.0 / adc_signal - 1);
+  
+  Serial.print("ADC signal ");
+  Serial.println(adc_signal);
+
+  Serial.print("Signal in volts ");
+  Serial.println(adc_to_volt);
+
+  delay(1000);
+
+}
