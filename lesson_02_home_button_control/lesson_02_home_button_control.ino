ce9a0e62b5ea8e14bd82ed49345bb83942f02a8c
Alexey <zhuravel.lesha2005@gmail.com>
2020-11-07 09:30:54 +0300

    added new branch

--- lesson_02_home_button_control/lesson_02_home_button_control.ino
+++ lesson_02_home_button_control/lesson_02_home_button_control.ino
@@ -1,16 +1,24 @@
 void setup() {
   Serial.begin(9600);
- pinMode(10, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+
 
 }
 
 void loop() {
 
-  Serial.println(digitalRead(4));
+  Serial.println(digitalRead(2));
   delay(500);
 
-  if (digitalRead(5) == 1) (
-      digitalWrite(10, HIGH));
+  if (digitalRead(5) == 1) {
+
+    digitalWrite(10, HIGH);
+    delay(500);
+  };
+
+
 
 
 
